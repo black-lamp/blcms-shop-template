d0bab2ae37e33eb08adcb547409c54fcc371dabb
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-11-25 17:09:52 +0200

    Changed init migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -16,10 +16,37 @@ class m130524_201442_init extends Migration
             'created_at' => time(),
             'updated_at' => time()
         ]);
+
+        $this->insert('language', [
+            'name' => 'Russian',
+            'lang_id' => 'ru',
+            'show' => true,
+            'active' => true,
+            'default' => true
+        ]);
+
+        $this->insert('language', [
+            'name' => 'Ukrainian',
+            'lang_id' => 'uk',
+            'show' => true,
+            'active' => true,
+            'default' => false
+        ]);
+
+        $this->update('language', [
+            'default' => false
+        ], ['lang_id' => 'en-US']);
     }
 
     public function down()
     {
         $this->delete('user', ['username' => 'admin']);
+
+        $this->delete('language', ['lang_id' => 'ru']);
+        $this->delete('language', ['lang_id' => 'uk']);
+
+        $this->update('language', [
+            'default' => true
+        ], ['lang_id' => 'en-US']);
     }
 }
