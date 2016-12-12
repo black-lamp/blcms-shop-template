a12a36dade1dd04219bda58d6a7fe6a1ba2a1352
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-12-12 13:19:47 +0200

    Added params

--- common/config/params.php
+++ common/config/params.php
@@ -3,6 +3,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'infoEmail' => 'info@example.com',
     'supportEmail' => 'support@example.com',
+    'orderManagerEmail' => 'orderManager@example.com',
+    'senderEmail' => 'sender@example.com',
     'partnerManagerEmail' => 'partnerManager@example.com',
+
     'user.passwordResetTokenExpire' => 3600,
 ];
